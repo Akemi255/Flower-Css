abb958f442f05365b08734ec0bf6ab8a758b01ca
gusvet18 <gus1465@yahoo.com>
2024-06-07 14:10:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// https://freecodez.com
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.getElementById("body").classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
